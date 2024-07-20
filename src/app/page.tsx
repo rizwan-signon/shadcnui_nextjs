eb6578555d1202da2f4b6bfa4ba1248c16547fad
your-username <your-email>
2024-07-20 15:29:32 +0500

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
-
+"use client";
+import { Calendar } from "@/components/ui/calendar";
+import { Button } from "@/components/ui/button";
+import React from "react";
 export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
     <main className="">
       <h1>welcome to pizza shop</h1>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+      <Button variant="outline">Button</Button>
     </main>
   );
 }
