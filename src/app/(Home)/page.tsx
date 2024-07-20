773f7ed52d527c36981bac1fa10c6be8ea65b3fb
your-username <your-email>
2024-07-20 17:40:09 +0500

    add hero section

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <h1>this is page .tsx</h1>
-    </div>
+    <>
+      <section className=" bg-white">
+        <div className="container flex items-center justify-between py-24">
+          <div>
+            <h1 className="font-bold text-4xl leading-2">
+              super delicius Pizza <br />
+              <span className=" text-primary ">in 45 Minutes</span>
+            </h1>
+            <p className=" mt-3 leading-snug max-w-lg">
+              enjoy a free meal if your order takes more than 45 minutes
+            </p>
+            <Button
+              className=" bg-primary px-4 py-7 rounded-3xl mt-8 text-lg font-bold hover:bg-green-500"
+              variant="outline"
+            >
+              get your pizza now
+            </Button>
+          </div>
+          <div>
+            <Image
+              className=" rounded-3xl"
+              alt="pizza-image"
+              src={"/pizzaimage.jpg"}
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
